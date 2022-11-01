981db03e4642d68c1dcb4e2630c6c501c2bf80ef
Riyadh <8844262+laplaceon@users.noreply.github.com>
2022-11-01 12:25:20 -0400

    Added code to enable oversampling

--- data.py
+++ data.py
@@ -4,38 +4,54 @@ from datetime import datetime, timedelta
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 import pytz
-from random import randint
+from random import randint, randrange
 import torch
 import torch.nn.functional as F
 from math import floor, sqrt
 
 class ChartDataset(Dataset):
-    def __init__(self, files, tf=512, multiplier=1):
+    def __init__(self, files, tf=512, multiplier=1, bs=1):
         self.multiplier = multiplier
         self.tf = tf
+        self.next_len = int(tf * 0.25)
         self.mms = MinMaxScaler()
+        self.bs = bs
         self.files = [None] * len(files)
-        self.pump_range = [x[1] for x in files]
+        self.names = [file[0] for file in files]
+        self.pump_range = [(x[1][0], x[1][1]+60000) for x in files]
+        self.bounds = [None] * len(files)
 
         for i, file in enumerate(files):
             self.files[i] = pd.read_csv(file[0])[['timestamp', 'side', 'price', 'amount']]
             self.files[i]['side'] = self.files[i]['side'].apply(lambda x: 1 if x == 'buy' else 0)
 
+            bounds = self.files[i].index[(self.files[i]['timestamp'] >= self.pump_range[i][0]) & (self.files[i]['timestamp'] <= self.pump_range[i][1])].tolist()
+
+            self.bounds[i] = (bounds[0], bounds[-1])
+
     def __getitem__(self, i):
         idx = floor(i / self.multiplier)
 
-        x = randint(0, len(self.files[idx]) - self.tf)
+        if randrange(0, 10) > 2:
+            x = randint(0, len(self.files[idx]) - self.tf - 1 - self.next_len)
+        else:
+            try:
+                x = randint(self.bounds[idx][0], self.bounds[idx][-1])
+            except:
+                print(bounds, self.pump_range[idx], self.names[idx])
 
         vals = self.files[idx].iloc[x:x+self.tf].copy()
-
+        last_idx = x + self.tf + 1
+        next = self.files[idx].iloc[last_idx:last_idx+self.next_len].copy()
         final_ts = vals.values[-1][0]
-
         pumping = 1 if (final_ts >= self.pump_range[idx][0] and final_ts <= self.pump_range[idx][1]) else 0
 
         vals[['timestamp', 'price', 'amount']] = self.mms.fit_transform(vals[['timestamp', 'price', 'amount']])
+        next[['timestamp', 'price', 'amount']]= self.mms.transform(next[['timestamp', 'price', 'amount']])
 
         return {
             "seq": vals.values,
+            "next": next.values,
             "pumping": pumping
         }
 
@@ -59,10 +75,11 @@ def get_data_loaders(landmarks_file, charts_dir, bs=16, multiplier=1):
         end = date.replace(hour=end[0], minute=end[1])
         # end -= timedelta(seconds=15)
         landmarks_map.append((f"{charts_dir}{id}.csv", (int(start.timestamp()) * 1000, int(end.timestamp()) * 1000)))
+        # print((f"{charts_dir}{id}.csv", start, (int(start.timestamp()) * 1000, int(end.timestamp()) * 1000)))
 
     X_train, X_val = train_test_split(landmarks_map, train_size=0.7, random_state=42)
 
-    train_ds = ChartDataset(X_train, multiplier=multiplier)
-    val_ds = ChartDataset(X_val, multiplier=multiplier)
+    train_ds = ChartDataset(X_train, multiplier=multiplier, bs=bs)
+    val_ds = ChartDataset(X_val, multiplier=multiplier, bs=bs)
 
-    return DataLoader(train_ds, batch_size=bs, shuffle=True), DataLoader(val_ds, batch_size=bs*2)
+    return DataLoader(train_ds, batch_size=bs, shuffle=True), DataLoader(val_ds, batch_size=bs, shuffle=True)
--- train.py
+++ train.py
@@ -6,14 +6,10 @@ import numpy as np
 from torch.optim import Adam
 
 from data import get_data_loaders
-from model import PumpDiscriminator
+from model import PumpDiscriminator, PndModel
 
 from tqdm import tqdm
-from sklearn.metrics import f1_score, matthews_corrcoef, accuracy_score
-
-train_dl, val_dl = get_data_loaders('./data/chart_landmarks.csv', './data/charts/', bs=256, multiplier=128)
-model = PumpDiscriminator()
-opt = Adam(model.parameters(), lr=1e-3)
+from sklearn.metrics import f1_score, roc_auc_score, accuracy_score
 
 def compute_accuracy(pred, labels):
     pred = np.argmax(pred, axis=1)
@@ -21,45 +17,56 @@ def compute_accuracy(pred, labels):
     return {
         "acc": accuracy_score(labels, pred),
         "f1": f1_score(labels, pred),
-        "mcc": matthews_corrcoef(labels, pred)
+        "roc": roc_auc_score(labels, pred)
     }
 
-for i in range(200):
-    model.eval()
-    valid_stats = 2 * [[0, 0, 0, 0]]
-    for batch in tqdm(val_dl, position=0, leave=True):
-        with torch.no_grad():
-            inputs = batch['seq'].float()
-            labels = batch['pumping'].long()
+def train_loop(model, opt, train_dl, val_dl, epochs):
+    for i in range(epochs):
+        model.eval()
+        valid_stats = 1 * [[0, 0, 0, 0]]
+        for batch in tqdm(val_dl, position=0, leave=True):
+            with torch.no_grad():
+                inputs = batch['seq'].float().cuda()
+                next = batch['next'].float().cuda()
+                labels = batch['pumping'].long().cuda()
 
-            preds = model(inputs)
+                preds = model(inputs, next)
+                # print(labels)
 
-            loss_0 = F.cross_entropy(preds, labels)
+                loss_0 = F.cross_entropy(preds[0], labels)
 
-            scores = compute_accuracy(preds.detach().cpu().numpy(), labels.detach().cpu().numpy())
+                scores = compute_accuracy(preds[0].detach().cpu().numpy(), labels.detach().cpu().numpy())
 
-            valid_stats[0] = [a + b for a, b in zip(valid_stats[0], [loss_0.item(), scores["acc"], scores["f1"], scores["mcc"]])]
-            valid_stats[1] = [a + b for a, b in zip(valid_stats[1], [0, 0, 0, 0])]
-    nb = len(val_dl)
-    for i in valid_stats:
-        print(f'Avg. loss: {i[0] / nb}, Acc: {i[1] / nb}, F1: {i[2] / nb}, MCC: {i[3] / nb}')
-    # return (valid_stats[0][0] + valid_stats[1][0]) / nb
+                valid_stats[0] = [a + b for a, b in zip(valid_stats[0], [loss_0.item(), scores["acc"], scores["f1"], scores["roc"]])]
+                # valid_stats[1] = [a + b for a, b in zip(valid_stats[1], [0, 0, 0, 0])]
+        nb = len(val_dl)
+        for i in valid_stats:
+            print(f'Valid loss: {i[0] / nb}, Acc: {i[1] / nb}, F1: {i[2] / nb}, MCC: {i[3] / nb}')
+        # return (valid_stats[0][0] + valid_stats[1][0]) / nb
 
-    model.train()
-    training_loss = 0
-    for batch in tqdm(train_dl, position=0, leave=True):
-        opt.zero_grad()
+        model.train()
+        training_loss = 0
+        for batch in tqdm(train_dl, position=0, leave=True):
+            opt.zero_grad()
 
-        inputs = batch['seq'].float()
-        labels = batch['pumping'].long()
+            inputs = batch['seq'].float().cuda()
+            next = batch['next'].float().cuda()
+            labels = batch['pumping'].long().cuda()
 
-        preds = model(inputs)
-        loss = F.cross_entropy(preds, labels)
+            preds = model(inputs, next)
+            loss = F.cross_entropy(preds[0], labels) + F.mse_loss(preds[1].permute(1, 0, 2), next)
 
-        training_loss += loss.item()
+            training_loss += loss.item()
 
-        loss.backward()
-        opt.step()
+            loss.backward()
+            opt.step()
 
-    nb = len(train_dl)
-    print(f"Training loss: {training_loss/nb}")
+        nb = len(train_dl)
+        print(f"Training loss: {training_loss/nb}")
+
+train_dl, val_dl = get_data_loaders('./data/chart_landmarks.csv', './data/charts/', bs=128, multiplier=256)
+model = PndModel()
+model.cuda()
+opt = Adam(model.parameters(), lr=1e-4)
+
+train_loop(model, opt, train_dl, val_dl, 200)
